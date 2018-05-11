266fb07b57d6225f188ddc3c54c0e03b2a824eb7
ThaoIE3 <37399712+ThaoIE3@users.noreply.github.com>
2018-05-11 09:36:16 +0700

    Add files via upload

---
+++ DieuKhien_DiChuyen.h
@@ -0,0 +1,9 @@
+#ifndef DIEUKHIEN_DICHUYEN_H_INCLUDED
+#define DIEUKHIEN_DICHUYEN_H_INCLUDED
+
+#include"struct.h"
+#include"console.h"
+
+void DieuKhien_DiChuyen(Snake &snake);
+
+#endif // DIEUKHIEN_DICHUYEN_H_INCLUDED
